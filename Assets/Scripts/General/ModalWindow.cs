edb672d613abae9bb8b915ae7cae1274ec36d78d
Tsatsenko Sergey <Sergey1996n71@gmail.com>
2023-05-03 16:54:25 +0300

    Added animation check

--- Assets/Scripts/General/ModalWindow.cs
+++ Assets/Scripts/General/ModalWindow.cs
@@ -7,23 +7,35 @@ using UnityEngine.UI;
 public class ModalWindow : MonoBehaviour
 {
     [SerializeField] protected Button _close;
+    private bool isAnumation = true;
 
     protected void Awake()
     {
         _close.onClick.AddListener(OnClose);
         gameObject.GetComponent<Button>().onClick.AddListener(OnClose);
+        isAnumation = PlayerPrefs.GetInt("Animation", 1) == 1;
     }
     protected void OnEnable()
     {
-        transform.GetChild(0).localScale = Vector3.one;
-        transform.GetChild(0).DOPunchScale(Vector3.one / 10, .5f, 1, 0);
+        if (isAnumation)
+        {
+            transform.GetChild(0).localScale = Vector3.one;
+            transform.GetChild(0).DOPunchScale(Vector3.one / 10, .5f, 1, 0);
+        }
     }
 
     protected void OnClose() 
-    { 
-        transform.GetChild(0).DOScale(Vector3.zero, .2f).OnComplete(() =>
+    {
+        if (isAnumation)
+        {
+            transform.GetChild(0).DOScale(Vector3.zero, .2f).OnComplete(() =>
+            {
+                gameObject.SetActive(false);
+            });
+        }
+        else
         {
             gameObject.SetActive(false);
-        });
+        }
     }
 }
