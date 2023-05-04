5c63107efa3e7bd7b2e6af48f65a721e755bf797
Tsatsenko Sergey <Sergey1996n71@gmail.com>
2023-05-04 11:33:01 +0300

    Fix animation in ModalWindows

--- Assets/Scripts/General/ModalWindow.cs
+++ Assets/Scripts/General/ModalWindow.cs
@@ -7,26 +7,29 @@ using UnityEngine.UI;
 public class ModalWindow : MonoBehaviour
 {
     [SerializeField] protected Button _close;
-    private bool isAnumation = true;
+    private bool isAnimation => PlayerPrefs.GetInt("Animation", 1) == 1;
 
     protected void Awake()
     {
         _close.onClick.AddListener(OnClose);
         gameObject.GetComponent<Button>().onClick.AddListener(OnClose);
-        isAnumation = PlayerPrefs.GetInt("Animation", 1) == 1;
     }
     protected void OnEnable()
     {
-        if (isAnumation)
+        if (isAnimation)
         {
             transform.GetChild(0).localScale = Vector3.one;
             transform.GetChild(0).DOPunchScale(Vector3.one / 10, .5f, 1, 0);
+        }
+        else
+        {
+            transform.GetChild(0).localScale = Vector3.one;
         }
     }
 
     protected void OnClose() 
     {
-        if (isAnumation)
+        if (isAnimation)
         {
             transform.GetChild(0).DOScale(Vector3.zero, .2f).OnComplete(() =>
             {
